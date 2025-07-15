7b750d4b696b08713139e707dab79f77ab377358
bangln <98367898+bangln@users.noreply.github.com>
2025-07-15 16:45:47 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+import argostranslate.package, argostranslate.translate
+from pathlib import Path
+import urllib.request
+
+# Tải và cài gói model en ↔ vi
+url = "https://www.argosopentech.com/argospm/index/translate-en_vi.argosmodel"
+package_path = Path("translate-en_vi.argosmodel")
+urllib.request.urlretrieve(url, package_path)
+argostranslate.package.install_from_path(package_path)
 python3 -m libretranslate
